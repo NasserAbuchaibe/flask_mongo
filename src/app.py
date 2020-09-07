d0ddda909d4ff3a25345bbf09b9104029f658e32
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-09-07 16:05:14 -0500

    CRUD

--- src/app.py
+++ src/app.py
@@ -1,11 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, Response
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
+from bson import json_util
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config['MONGO_URI']='mongodb://localhost/pythonmongodb'
 
 mongo = PyMongo(app)
+
 
 @app.route('/users', methods=['POST'])
 def create_user():
@@ -26,7 +29,56 @@ def create_user():
         }
         return response
     else:
-        pass
-    
+        return not_found()
+
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = mongo.db.users.find()
+    response = json_util.dumps(users)
+    return Response(response, mimetype='aplication/json')
+
+
+@app.route('/users/<id>', methods=['GET'])
+def get_user(id):
+    user = mongo.db.users.find_one({'_id': ObjectId(id)})
+    response = json_util.dumps(user)
+    return Response(response, mimetype='application/json')
+
+
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    mongo.db.users.delete_one({'_id': ObjectId(id)})
+    response = jsonify({'message': 'User ' + id + ' was deleted successfully'})
+    return response
+
+
+@app.route('/users/<id>', methods=['PUT'])
+def update_user(id):
+    username = request.json['username']
+    email = request.json['email']
+    password = request.json['password']
+
+    if username and email and password:
+        hashed_password = generate_password_hash(password)
+        mongo.db.users.update_one({'_id': ObjectId(id)}, {'$set': {
+            'username': username,
+            'passwors': hashed_password,
+            'email': email
+        }})
+    response = jsonify({'message': 'User ' + id + ' was updated successfully'})
+    return response
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    response = jsonify({
+        'message': 'Resource Not Found ' + request.url,
+        'status': 404
+    })
+    response.status_code = 404
+    return response
+
+
 if __name__ == "__main__":
     app.run(debug=True)
